c313a0c36a2cc56999f018cf2d43dd583d0baf71
Stef Ruinard <stefruinard@hotmail.com>
2021-05-29 09:49:59 +0200

    test pipeline.

--- build_and_delete.sh
+++ build_and_delete.sh
@@ -1,2 +1,34 @@
+#!/bin/bash
 echo You Are rocking this script
-az group create --resource-group mynewresourcegroup --location westeurope
+FOLDERS_TO_CREATE=("folder_to_delete_1" "folder_to_delete_2" "folder_to_keep")
+FOLDERS_TO_DELETE=("folder_to_delete_1" "folder_to_delete_2")
+CONTAINER_NAME="datalake"
+RESOURCE_GROUP="rabosrrg"
+STORAGE_ACCOUNT="rabosrsa${RANDOM}"
+az group create \
+	--resource-group $RESOURCE_GROUP \
+	--location westeurope
+
+az storage account create \
+	--name $STORAGE_ACCOUNT \
+	--resource-group $RESOURCE_GROUP \
+	--location westeurope \
+	--sku Standard_LRS 
+
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT
+for folder_name in ${FOLDERS_TO_CREATE[@]}
+do
+	echo 'creating folder:' $folder_name 'in container ' $CONTAINER_NAME
+	az storage blob upload \
+		--account-name $STORAGE_ACCOUNT \
+		--container-name $CONTAINER_NAME \
+		--name ${folder_name}/folder2/basic_file.txt \
+		--file basic_file.txt
+done
+
+for folder_name in ${FOLDERS_TO_DELETE[@]}
+do
+	echo 'deleting folder:' $folder_name 'in container ' $CONTAINER_NAME
+	az storage blob delete-batch --account-name $STORAGE_ACCOUNT -s $CONTAINER_NAME --pattern ${folder_name}/*
+done
+
