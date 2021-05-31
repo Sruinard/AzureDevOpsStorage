ac4dc9bc71593c67d15234a8983dd3f7d977854f
Stef Ruinard <stefruinard@hotmail.com>
2021-05-31 06:28:37 +0200

    Final pipeline.

--- build_and_delete.sh
+++ build_and_delete.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo You Are rocking this script
+
+echo "Defining Constants"
 FOLDERS_TO_CREATE=("folder_to_delete_1" "folder_to_delete_2" "folder_to_keep")
 FOLDERS_TO_DELETE=("folder_to_delete_1" "folder_to_delete_2")
 CONTAINER_NAME="datalake"
 RESOURCE_GROUP="rabosrrg"
 STORAGE_ACCOUNT="rabosrsa${RANDOM}"
+
+echo "Creating Resource Group and Storage account"
 az group create \
 	--resource-group $RESOURCE_GROUP \
 	--location westeurope
@@ -15,6 +18,7 @@ az storage account create \
 	--location westeurope \
 	--sku Standard_LRS 
 
+echo "Creating storage container"
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT
 for folder_name in ${FOLDERS_TO_CREATE[@]}
 do
@@ -26,6 +30,7 @@ do
 		--file basic_file.txt
 done
 
+echo "Deleting subset of folders in Storage container"
 for folder_name in ${FOLDERS_TO_DELETE[@]}
 do
 	echo 'deleting folder:' $folder_name 'in container ' $CONTAINER_NAME
